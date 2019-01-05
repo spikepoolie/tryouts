78139466b7e0e674bad3b60139c721b259f72c40
Rodrigo Schreiner <clintscoob@hotmail.com>
2019-01-04 21:22:18 -0800

    Initial Commit

---
+++ Tryouts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tryouts
+//
+//  Created by Rodrigo Schreiner on 1/4/19.
+//  Copyright © 2019 GoEmobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
