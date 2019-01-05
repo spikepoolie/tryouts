303828d00f8e4e085a8be2c3cec6ee2fffefb837
Rodrigo Schreiner <clintscoob@hotmail.com>
2019-01-04 21:54:00 -0800

    main page calling create account page

---
+++ Tryouts/NewAccount.swift
@@ -0,0 +1,30 @@
+//
+//  NewAccount.swift
+//  Tryouts
+//
+//  Created by Rodrigo Schreiner on 1/4/19.
+//  Copyright © 2019 GoEmobile. All rights reserved.
+//
+
+import UIKit
+
+class NewAccount: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
